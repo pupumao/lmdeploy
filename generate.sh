b5e0a41aa0949bf08c5d0d80a8c49736e6c03f98
tianrunhe <tianrunhe@meituan.com>
2024-05-13 10:47:31 +0800

    make in dolphinfs

--- generate.sh
+++ generate.sh
@@ -2,11 +2,16 @@
 
 builder="-G Ninja"
 
+rapidjson_root='/mnt/dolphinfs/ssd_pool/docker/user/hadoop-automl/tianrunhe/packages/rapidjson-release'
+rapidjson_inc=${rapidjson_root}/include
+
 if [ "$1" == "make" ]; then
     builder=""
 fi
 
+RapidJSON_DIR=${rapidjson_root} \
 cmake ${builder} .. \
+    -DCMAKE_CXX_FLAGS=-I${rapidjson_inc} \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
     -DCMAKE_INSTALL_PREFIX=./install \
